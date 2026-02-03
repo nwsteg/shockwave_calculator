69cffdc09cdacbb87432b1fd67741266ad970601
nwsteg <75855220+nwsteg@users.noreply.github.com>
2026-02-03 15:37:16 -0600

    Fix oblique shock angle solver

---
+++ App.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar } from "expo-status-bar";
+import IsentropicScreen from "./src/screens/IsentropicScreen";
+import NormalShockScreen from "./src/screens/NormalShockScreen";
+import ObliqueShockScreen from "./src/screens/ObliqueShockScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar style="dark" />
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Isentropic" component={IsentropicScreen} />
+        <Tab.Screen name="Normal Shock" component={NormalShockScreen} />
+        <Tab.Screen name="Oblique Shock" component={ObliqueShockScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
