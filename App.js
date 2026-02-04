b62ddc99765dfbdef0ec72de2040b68a69b2d243
nwsteg <75855220+nwsteg@users.noreply.github.com>
2026-02-03 22:19:15 -0600

    Add paywall UI scaffolding

---
+++ App.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import CondensationScreen from "./src/screens/CondensationScreen";
+import IsentropicScreen from "./src/screens/IsentropicScreen";
+import NormalShockScreen from "./src/screens/NormalShockScreen";
+import ObliqueShockScreen from "./src/screens/ObliqueShockScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="dark" />
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Isentropic" component={IsentropicScreen} />
+          <Tab.Screen name="Normal Shock" component={NormalShockScreen} />
+          <Tab.Screen name="Oblique Shock" component={ObliqueShockScreen} />
+          <Tab.Screen name="Condensation" component={CondensationScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
