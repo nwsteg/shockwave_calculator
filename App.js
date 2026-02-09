edbc6e6ed07d52207b450cb2283a4b4f05ba2b0b
nwsteg <75855220+nwsteg@users.noreply.github.com>
2026-02-09 12:38:23 -0600

    Fix reanimated configuration for drawer

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StatusBar } from "expo-status-bar";
